76aaa785fa036477bc528668ec318a112d8f819e
Anna Carollyne <anna.silva@infortechms.com.br>
2025-05-13 08:25:26 -0400

    Adicionar arquivos de projeto.

---
+++ AnnaPortifolio/wwwroot/js/Index.js
@@ -0,0 +1,12 @@
+﻿function alternarIdioma() {
+    const textoPT = document.getElementById("texto-pt");
+    const textoEN = document.getElementById("texto-en");
+
+    if (textoPT.style.display === "none") {
+        textoPT.style.display = "block";
+        textoEN.style.display = "none";
+    } else {
+        textoPT.style.display = "none";
+        textoEN.style.display = "block";
+    }
+}
