2d3f03fa1ac806d1ed0455dd8838829681569b64
Anna Carollyne <anna.silva@infortechms.com.br>
2025-05-13 14:38:16 -0400

    Adicionado botão e implementação de alteração de tema

--- AnnaPortifolio/wwwroot/js/Index.js
+++ AnnaPortifolio/wwwroot/js/Index.js
@@ -10,3 +10,15 @@
         textoEN.style.display = "block";
     }
 }
+
+const toggleTheme = () => {
+    document.body.classList.toggle('dark-theme');
+    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
+};
+
+window.onload = () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+};
